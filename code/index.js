ce18af70f740b0e598bee14500ad5e74d902a4b4
nicolasmarcosojeda <marcosnicolass.74@gmail.com>
2023-07-09 18:02:23 -0300

    JavaScript - Programa del calculo de stock

---
+++ code/index.js
@@ -0,0 +1,82 @@
+function calcularStock(cantidadDisponible, cantidadVendida, cantidadConsumida) {
+  let stock = cantidadDisponible - cantidadVendida - cantidadConsumida;
+
+  if (stock >= 0) {
+    return stock;
+  } else {
+    return "No hay suficiente stock";
+  }
+}
+
+function obtenerNumeroIngresado(mensaje) {
+  let numero = parseInt(prompt(mensaje));
+
+  while (isNaN(numero) || numero <= 0) {
+    numero = parseInt(prompt("Valor inválido. Por favor, ingrese un número válido y mayor a cero:"));
+  }
+
+  return numero;
+}
+
+let cervezas = [];
+
+// Obtener las cervezas almacenadas en localStorage
+let cervezasGuardadas = localStorage.getItem('cervezas');
+
+// Si hay cervezas almacenadas, convertir el JSON a un array
+if (cervezasGuardadas) {
+  cervezas = JSON.parse(cervezasGuardadas);
+}
+
+let cantidadCervezasInput = document.getElementById("cantidadCervezas");
+let agregarCervezaButton = document.getElementById("agregarCerveza");
+let resultadoDiv = document.getElementById("resultado");
+
+function agregarCerveza() {
+  let nombreCervezaInput = prompt("Ingrese el nombre de la cerveza:");
+  let cantidadDisponible = obtenerNumeroIngresado("Ingrese la cantidad disponible:");
+  let cantidadVendida = obtenerNumeroIngresado("Ingrese la cantidad vendida:");
+  let cantidadConsumida = obtenerNumeroIngresado("Ingrese la cantidad consumida:");
+
+  let cerveza = {
+    nombre: nombreCervezaInput,
+    cantidadDisponible: cantidadDisponible,
+    cantidadVendida: cantidadVendida,
+    cantidadConsumida: cantidadConsumida
+  };
+
+  cervezas.push(cerveza);
+
+  // Guardar las cervezas en localStorage como JSON
+  localStorage.setItem('cervezas', JSON.stringify(cervezas));
+
+  mostrarStockCervezas();
+
+  cantidadCervezasInput.value = "";
+  agregarCervezaButton.disabled = true;
+}
+
+function mostrarStockCervezas() {
+  resultadoDiv.innerHTML = ""; // Limpiar contenido anterior
+
+  cervezas.forEach((cerveza, index) => {
+    cerveza.stockCalculado = calcularStock(cerveza.cantidadDisponible, cerveza.cantidadVendida, cerveza.cantidadConsumida);
+
+    let p = document.createElement("p");
+    p.textContent = `El stock de la cerveza ${cerveza.nombre} es: ${cerveza.stockCalculado}`;
+    resultadoDiv.appendChild(p);
+  });
+}
+
+cantidadCervezasInput.addEventListener("input", function() {
+  let cantidadCervezas = parseInt(cantidadCervezasInput.value);
+
+  if (isNaN(cantidadCervezas) || cantidadCervezas <= 0) {
+    agregarCervezaButton.disabled = true;
+  } else {
+    agregarCervezaButton.disabled = false;
+  }
+});
+
+agregarCervezaButton.addEventListener("click", agregarCerveza);
+
